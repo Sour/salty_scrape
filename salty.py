f10c8aba1fd0f6acb8ea81d82adfcbd76c72b04f
William Gunter <in.wil.i.trust@gmail.com>
2017-02-12 19:39:52 -0800

    collection data

--- salty.py
+++ salty.py
@@ -6,8 +6,6 @@ urlWEB = "http://www.saltybet.com/"
 urlLOGIN = 'http://www.saltybet.com/authenticate?signin=1'
 urlBET = 'http://www.saltybet.com/ajax_place_bet.php'
 
-win_pct = {'win':0,'loss':0};
-
 login_payload = {
     'email': "mechanicfreak440@gmail.com",
     'pword': 'lolice440',
@@ -39,91 +37,102 @@ def saveBettingData():
 
 def openBettingData():
     with open('betting_data.pickle', 'rb') as f:
-        return pickle.load(f)  
+        return pickle.load(f)
 
-def waitForNewData(s):
+def waitForMatchEnd(s):
+    print("\nWaiting for bet start")
     data = requests.get(urlJSON).json()
-    while data['status'] != 'open':
+    while data['status'] == 'locked':
         data = requests.get(urlJSON).json()
         time.sleep(2)
-        print("Waiting for bet start")
     time.sleep(3)
     return data
 
 def waitForMatchStart():
+    print("\nWaiting for match start")
     data = requests.get(urlJSON).json()
     while data['status'] != 'locked':
         data = requests.get(urlJSON).json()
         time.sleep(2)
-        print("Waiting for match start")
     return
 
 def getBalance(r):
     tree = html.fromstring(r.content)
     return tree.xpath('//span[@class="dollar"]/text()')[0]
 
-def bet(data):
+def bet(p1,p2):
     
-   # if(p1):
-    bet_payload['selectedplayer'] = 'player1'
-   # else
-       # bet_payload['player2'] = data['p2name']
+    if p1 in betting_data:
+        count_p1 = betting_data[p1].count(p2)
 
+    if p2 in betting_data:
+        count_p2 = betting_data[p2].count(p2)
+
+    if count_p1 >= count_p2:
+        bet_payload['selectedplayer'] = 'player1'
+    else:
+        bet_payload['selectedplayer'] = 'player2'
 
 betting_data = openBettingData()
 
 s = requests.session()
-
 results = s.get(urlLOGIN)
+
+#login
 results = s.post(urlLOGIN, login_payload, dict(referer = urlLOGIN))
 
 data = requests.get(urlJSON).json()
 balance = getBalance(results)
 
-while(True):
-    
+p1 = data['p1name']
+p2 = data['p2name']
+
+if p1 not in betting_data:
+    betting_data[p1] = []
+if p2 not in betting_data:
+    betting_data[p2] = []
+
+while(True):  
+    data = waitForMatchEnd(s)
+
     results = s.get(urlWEB)
     
+    balance = getBalance(results)
 
-    balance_new = getBalance(results)
+    if data['status'] == '1':
+        win_pct['win'] = win_pct['win'] + 1
+        betting_data[p1].append(p2)
+        print(p1," won!")
+               
+    if data['status'] == '2':
+        win_pct['loss'] = win_pct['loss'] + 1
+        betting_data[p2].append(p1)
+        print(p2," won!")
     
-    print(betting_data)
+    saveBettingData()
+    
+    data = requests.get(urlJSON).json()
     
     p1 = data['p1name']
     p2 = data['p2name']
+    if p1 not in betting_data:
+        betting_data[p1] = []
+    if p2 not in betting_data:
+        betting_data[p2] = []
+        
+    bet(p1,p2)
+    print(data['p1name'], " vs ", data['p2name'])
+    print(p1,": ",betting_data[p1])
+    print(p2,": ",betting_data[p2])
     
-    if balance < balance_new:
-        win_pct['win'] = win_pct['win'] + 1
-        if p1 not in betting_data:
-            betting_data[p1] = []
-            
-        betting_data[p1].append(p2)
-    elif balance > balance_new:
-        win_pct['loss'] = win_pct['loss'] + 1
-        if p2 not in betting_data:
-            betting_data[p2] = []
-            
-        betting_data[p2].append(p1)
-
-    balance = balance_new
-    
-    data = waitForNewData(s)
+    print("\nSent POST: ",bet_payload)
     
     r_bet = s.post(urlBET,bet_payload, headers)
-    print(r_bet.status_code, r_bet.reason)
-    print(getBalance(results))
-    print(win_pct)
+    
+    print("POST Response:", r_bet.status_code, r_bet.reason,"\n")
+
     print(betting_data)
     waitForMatchStart()
-    saveBettingData()
-
-
-#data = waitForNewData()
-
-
-#p1 = data['p1name']
-#p2 = data['p2name']
-#print(p1,p2)
 
 
 
